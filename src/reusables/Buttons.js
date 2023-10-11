f6201265e6b4a7ded07c314fe4add7796eca032c
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-10-11 16:19:46 +0200

    fixed responsive issues

--- src/reusables/Buttons.js
+++ src/reusables/Buttons.js
@@ -34,6 +34,38 @@ export const PrimaryButton = styled.button`
        
     }
     `;
+export const ArticleButton = styled.button`
+   
+    align-items: center;
+   flex-grow: 1;
+  text-align: center; // optional, to center the text
+    padding: 13px;
+    background: ${({ theme, variant }) => variant === 'alternative' ? theme.colors.lightGreenPastel : theme.colors.lightBlue};
+    border-radius: 20px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 27px;
+    border: none;
+    font-family: Futura;
+    text-decoration: none;
+   
+    margin: 10px 30px 15px 30px; 
+    max-width: 12rem;
+    border-top: 1px solid black;
+
+    &:hover {
+    background: ${({ theme }) => theme.colors.lightBluePastel};
+    color: black;
+    border-bottom: 2px solid black;
+
+    }
+    @media screen and (min-width: 668px) {
+        margin: 1.25rem 3rem 1.875rem 0;
+       
+    }
+    `;
+
 
 
 
