b967244fb9373311eb96b6a696b403e6acc6b452
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-02-26 15:28:33 +0100

    updated techstack

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -13,19 +13,18 @@ const TechStack = () => {
             <SectionTitle>Tech Stack</SectionTitle>
             <div>
           <OnLightText>
-                    React.js, JavaScript,
-                    <BlueBackgroundText> Node.js,
-                    </BlueBackgroundText>
-                    Redux,
-                    <PinkBackgroundText>
-                    </PinkBackgroundText> Git, GitHub, Visual Studio Code,
+                    React.js, JavaScript/TypeScript, Next.js,
+                    <BlueBackgroundText> Node.js
+                    </BlueBackgroundText>, <LightBlueBackgroundText>   Redux  </LightBlueBackgroundText>
+,
+                      Sanity.io, Contentful, WebFlow,
+                    <PinkBackgroundText> Git, GitHub, Visual Studio Code, </PinkBackgroundText>
 
                     <PinkBackgroundText>Express.js, MongoDB, </PinkBackgroundText>Styled-components, React Router,<LightBlueBackgroundText> Mongoose </LightBlueBackgroundText>
                     RESTful APIs, CSS3, <BlueBackgroundText>HTML5, ES6, </BlueBackgroundText>SASS, NPM/Yarn, Webpack,
                     <PinkBackgroundText> Agile Methodologies, </PinkBackgroundText>
-                    MongoDB, <LightBlueBackgroundText>Postman, Jest, CI/CD,   </LightBlueBackgroundText> Web Accessibly, Responsive Design, <BlueBackgroundText>Pair-Programming,</BlueBackgroundText> Code Reviews
-                    , Team Collaboration,
-            </OnLightText>
+                    MongoDB, <LightBlueBackgroundText>Postman, Jest, CI/CD,   </LightBlueBackgroundText> Web Accessibility, Responsive Design, <BlueBackgroundText>Pair-Programming,</BlueBackgroundText> Code Reviews
+            </OnLightText> <PinkBackgroundText>Unit Testing</PinkBackgroundText>
           </div>
         </InnerWrapper>
       </SectionWrapper>
